a5ee1dc618ddd195ff5297cac1a05fc416ec711e
Amal Shukla <shuklamal94@gmail.com>
2017-01-17 14:01:55 +0530

    package json , webconfig added,gitignore added

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+var webpack = require("webpack");
+
+module.exports = {
+	entry: './src/index.js',
+	output: {
+		path: 'dist/assests',
+		filename: 'bundle.js',
+		publicPath: 'assests'
+	},
+	devServer: {
+		inline: true,
+		contentBase: './dist',
+		port: 3000
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: ['babel-loader'],
+				query: {
+					presets: ['latest', 'react', 'stage-0']
+				}
+			},
+			{
+				test: /\.json$/,
+				exclude: /node_modules/,
+				loader: 'json-loader'
+			},
+			{
+				test: /\.css$/,
+				loader: 'style-loader!css-loader!autoprefixer-loader'
+			},
+			{
+				test: /\.scss$/,
+				loader: 'style-loader!css-loader!autoprefixer-loader!sass-loader'
+			}
+		]
+	}
+}
